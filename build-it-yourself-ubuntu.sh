1998a47a8f3285840f5f4b23e7515415a6f92da5
Lucas Puntillo <62703475+puntillol59@users.noreply.github.com>
2020-11-30 18:30:13 +0000

    Add files via upload

---
+++ build-it-yourself-ubuntu.sh
@@ -0,0 +1,15 @@
+# Build the distro yourself
+echo "You need go installed, without it, the LXC container will not build."
+sleep 20
+mkdir $HOME/distrobuilder-lxc
+cd $HOME/distrobuilder-lxc
+wget https://linuxcontainers.org/downloads/distrobuilder/distrobuilder-1.1.tar.gz --directory-prefix=$HOME/distrobuilder-lxc
+ls
+tar -xvf $HOME/distrobuilder-lxc/distrobuilder-1.1.tar.gz
+sudo apt install snapd
+sudo snap install distrobuilder --edge --classic
+mkdir -p ContainerImages/distro/
+cd ContainerImages/distro/
+curl -O https://raw.githubusercontent.com/lxc/lxc-ci/master/images/alpine.yaml
+sudo distrobuilder build-lxc alpine.yaml -o image.release=3.10
+ls
